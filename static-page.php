091a0c33f196400303dd5b5afd7e7bd0d9bee1fb
Tuan Nguyen <tuannh1090@gmail.com>
2020-02-12 15:26:23 +0700

    them file static Page

---
+++ static-page.php
@@ -0,0 +1,174 @@
+<?php include 'header.php';?>
+<!-- Hero Start -->
+        <section class="bg-half bg-light">
+            <div class="home-center">
+                <div class="home-desc-center">
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-lg-12 text-center">
+                                <div class="page-next-level">
+                                    <h2> Tính Năng CRMLand thật bá đạo </h2>
+                                    <ul class="list-unstyled mt-4">
+                                        <li class="list-inline-item h6 date text-muted"><i class="mdi mdi-calendar-check"></i> 13th August, 2019</li>
+                                    </ul>
+                                    <ul class="page-next d-inline-block bg-white shadow p-2 pl-4 pr-4 rounded mb-0">
+                                        <li><a href="index.html" class="text-uppercase font-weight-bold text-dark">Trang chủ</a></li>
+                                        <li>
+                                            <span class="text-uppercase text-primary font-weight-bold">Tính năng CRMLAND</span> 
+                                        </li> 
+                                    </ul>
+                                </div>
+                            </div>  <!--end col-->
+                        </div><!--end row-->
+                    </div> <!--end container-->
+                </div>
+            </div>
+        </section><!--end section-->
+        <!-- Hero End -->
+
+        <!-- Shape Start -->
+        <div class="position-relative">
+            <div class="shape overflow-hidden text-white">
+                <svg viewBox="0 0 2880 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M0 48H1437.5H2880V0H2160C1442.5 52 720 0 720 0H0V48Z" fill="currentColor"></path>
+                </svg>
+            </div>
+        </div>
+        <!--Shape End-->
+
+        <!-- Blog STart -->
+        <section class="section">
+            <div class="container">
+                <div class="row">
+                    <!-- START SIDEBAR -->
+                    <div class="col-lg-4 col-md-5 col-12 mt-4 mt-sm-0 pt-2 pt-sm-0">
+                        <div class="sidebar mt-sm-30 p-4 rounded shadow">
+                            <!-- SEARCH -->
+                            <div class="widget mb-4 pb-2">
+                                <h4 class="widget-title">Search</h4>
+                                <div id="search2" class="widget-search mt-4 mb-0">
+                                    <form role="search" method="get" id="searchform" class="searchform">
+                                        <div>
+                                            <input type="text" class="border rounded" name="s" id="s" placeholder="Search Keywords...">
+                                            <input type="submit" id="searchsubmit" value="Search">
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                            <!-- SEARCH -->
+
+                            <!-- CATAGORIES -->
+                            <div class="widget mb-4 pb-2">
+                                <h4 class="widget-title">Catagories</h4>
+                                <ul class="list-unstyled mt-4 mb-0 blog-catagories">
+                                     <li>
+                                        <a href="page-aboutus.html">
+                                            <i class="mdi mdi-chevron-right"></i> Quản lý dữ liệu khách hàng
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="page-services.html">
+                                            <i class="mdi mdi-chevron-right"></i> Tạo phếu thu thông tin khách hàng
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="page-services.html">
+                                            <i class="mdi mdi-chevron-right"></i> Quản lý nhật ký chăm sóc khách hàng
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="page-services.html">
+                                            <i class="mdi mdi-chevron-right"></i> Phân loại khách hàng thông minh
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="page-services.html">
+                                            <i class="mdi mdi-chevron-right"></i> Email Marketing
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="page-services.html">
+                                            <i class="mdi mdi-chevron-right"></i> SMS Marketing
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="page-services.html">
+                                            <i class="mdi mdi-chevron-right"></i> Chăm sóc khách hàng tự động
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="page-services.html">
+                                            <i class="mdi mdi-chevron-right"></i> Xây dựng Landing Page bất động sản
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="page-services.html">
+                                            <i class="mdi mdi-chevron-right"></i> Xây dựng webste bất động sản
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="page-services.html">
+                                            <i class="mdi mdi-chevron-right"></i> Công cụ website bất động sản
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="page-services.html">
+                                            <i class="mdi mdi-chevron-right"></i> Đồng bộ dữ liệu trên facebook ads
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="page-services.html">
+                                            <i class="mdi mdi-chevron-right"></i> Quản lý và nhắc nhở công việc
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="page-services.html">
+                                            <i class="mdi mdi-chevron-right"></i> Thông báo thông minh
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="page-services.html">
+                                            <i class="mdi mdi-chevron-right"></i> Bản tin định kỳ
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="page-services.html">
+                                            <i class="mdi mdi-chevron-right"></i> Brandname & Email theo tên miền
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <!-- CATAGORIES -->
+                        </div>
+                    </div><!--end col-->
+                    <!-- END SIDEBAR -->
+
+                    <!-- BLog Start -->
+                    <div class="col-lg-8 col-md-7">
+                        <div class="mr-lg-3">
+                            <div class="blog position-relative overflow-hidden shadow rounded">
+                                <div class="content p-4">
+                                    <h2>Đây là tiêu đề H2 </h2>
+                                    <h3>Đây là tiêu đề H3 </h3>
+                                    <h4>Đây là tiêu đề H4 </h4>
+                                    <h5>Đây là tiêu đề H5 </h5>
+                                    <h6>Đây là tiêu đề H6 </h6>
+                                    <p>The most well-known dummy text is the 'Lorem Ipsum', which is said to have originated in the 16th century. Lorem Ipsum is composed in a pseudo-Latin language which more or less corresponds to 'proper' Latin. It contains a series of real Latin words. This ancient dummy text is also incomprehensible, but it imitates the rhythm of most European languages in Latin script. </p>
+                                    <img src="images/blog/01.jpg" class="img-fluid rounded-top" alt="">
+                                    <blockquote class="blockquote mt-3 p-3">
+                                        <p class="text-muted mb-0 font-italic">" There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. "</p>
+                                    </blockquote>
+                                    <p>The advantage of its Latin origin and the relative meaninglessness of Lorum Ipsum is that the text does not attract attention to itself or distract the viewer's attention from the layout.</p>
+                                    <img src="images/blog/01.jpg"  alt="">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- BLog End -->
+
+                    
+                </div><!--end row-->
+            </div><!--end container-->
+        </section><!--end section-->
+        <!-- Blog End -->
+<?php include 'footer.php';?>
