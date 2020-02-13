d7b47153a0d7eebe86478c3dd229dc0d5ed8c27a
Tuan Nguyen <tuannh1090@gmail.com>
2020-02-13 15:06:51 +0700

    them file chi tiet mau web

---
+++ web-detail.php
@@ -0,0 +1,193 @@
+<?php include 'header.php';?>
+<!-- Hero Start -->
+        <section class="bg-half bg-light">
+            <div class="home-center">
+                <div class="home-desc-center">
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-lg-12 text-center">
+                                <div class="page-next-level">
+                                    <h1> Mấu website bất động sản 01 </h1>
+                                    <ul class="list-unstyled mt-4">
+                                        <li class="list-inline-item h6 date text-muted"><i class="mdi mdi-calendar-check"></i> 13th August, 2019</li>
+                                    </ul>
+                                    <ul class="page-next d-inline-block bg-white shadow p-2 pl-4 pr-4 rounded mb-0">
+                                        <li><a href="index.html" class="text-uppercase font-weight-bold text-dark">Trang chủ</a></li>
+                                        <li>
+                                            <span class="text-uppercase text-primary font-weight-bold">Thư viện mẫu website</span> 
+                                        </li> 
+                                        <li>
+                                            <span class="text-uppercase text-primary font-weight-bold">Landingpage dự án</span> 
+                                        </li> 
+                                    </ul>
+                                </div>
+                            </div>  <!--end col-->
+                        </div><!--end row-->
+                    </div> <!--end container-->
+                </div>
+            </div>
+        </section><!--end section-->
+        <!-- Hero End -->
+
+        <!-- Shape Start -->
+        <div class="position-relative">
+            <div class="shape overflow-hidden text-white">
+                <svg viewBox="0 0 2880 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M0 48H1437.5H2880V0H2160C1442.5 52 720 0 720 0H0V48Z" fill="currentColor"></path>
+                </svg>
+            </div>
+        </div>
+        <!--Shape End-->
+
+        <!-- Blog STart -->
+        <section class="section">
+            <div class="container">
+                <div class="row">
+                    <!-- START SIDEBAR -->
+                    <div class="col-lg-4 col-md-5 col-12 mt-4 mt-sm-0 pt-2 pt-sm-0">
+                        <div class="sidebar mt-sm-30 p-4 shadow">
+
+                            <!-- CATAGORIES -->
+                            <div class="widget mb-4 pb-2">
+                                <div class="" style="border-bottom: 1px solid #eee;">
+                                    <h4 class="text-primary">Mẫu giao diện landing page dự án 01</h4>
+                                    <p class="text-muted">Mã giao diện: #betovn</p>
+                                </div>
+                                <h4 class="text-danger mt-3 text-center">Miễn phí</h4>
+                                <div class="text-center pb-4" style="border-bottom: 1px solid #eee;">
+                                    <button class="d-block btn btn-success w-100 mb-3">XEM DEMO THỰC TẾ</button>
+                                    <span>Hoặc</span>
+                                    <button class="d-block btn btn-danger w-100 mt-3">TẠO WEBSITE VỚI MẪU NÀY</button>
+                                </div>
+                                <div class="mt-3">
+                                    <p>Bạn còn thắc mắc, vui lòng liên hệ</p>
+                                    <p class="font-weight-bold text-danger">Hotline: 1800 6730</p>
+                                    <p>Hoặc email: info@vtechhomes.vn</p>
+                                    <p>Tải về giao diện này, <a href="#">Click here</a></p>
+                                </div>    
+                            </div>
+                            <!-- CATAGORIES -->
+                        </div>
+                    </div><!--end col-->
+                    <!-- END SIDEBAR -->
+
+                    <!-- BLog Start -->
+                    <div class="col-lg-8 col-md-7">
+                        <div class="mr-lg-3">
+                            <div class="blog position-relative p-2">
+                                <img src="https://product.hstatic.net/1000012173/product/img01_0f78c1542b53433ab74a2344e226a6da_master.jpg" class="w-100">
+                               <div class="mt-3 content-web">
+                                   <p>
+                                        <strong>Landrick</strong> is a Powerful Saas &amp; Software Bootstrap Template. It is an excellent HTML template for startup, business, application, saas, marketing, agency, Hotel, Modern Business, Personal Portfolio, Services, Enterprise, Minimal Portfolio, Coworking Space, Paymens, Cryptocurrency, Software, Cloud Hosting, Event, Educational Course, Single Product etc.  It is 100% responsive and looks stunning on all types of screens and devices. Users will love your site because it gives them a unique user experience (UX), clean, modern &amp; beautiful design. They are also easy to customize and to combine with other components.<br><br>We have added a <strong>Dark version with RTL supported in both Light and Dark version</strong> and fixed couples of design issues. Added new landing pages Classic Application, Classic Saas and Car Ride Landing Page.<br><br>
+                                    </p>
+                                    <h2>Tính năng nổi trội</h2>
+                                    <div class="row">
+                                        <div class="col-lg-6 col-md-6 col-12 mt-4 pt-2">
+                                            <a class="text-dark" id="button-price-email-marketing">
+                                                <div class="key-feature d-flex p-3 bg-white rounded shadow">
+                                                    <div class="icons text-center rounded-pill mr-3 mt-2">
+                                                        <img src="images/mail.svg" class="avatar avatar-ex-sm" alt="">
+                                                    </div>
+                                                    <div class="content">
+                                                        <h4 class="title mb-0">100/100 điểm Google</h4>
+                                                        <p class="text-muted mb-0">Giúp website bạn nhanh chóng lên top từ khóa</p>    
+                                                    </div>
+                                                </div>
+                                            </a>
+                                        </div><!--end col-->
+                                        
+                                        <div class="col-lg-6 col-md-6 col-12 mt-4 pt-2">
+                                            <a class="text-dark" id="button-price-sms-marketing">
+                                                <div class="key-feature d-flex p-3 bg-white rounded shadow">
+                                                    <div class="icons text-center rounded-pill mr-3 mt-2">
+                                                        <img src="images/sms.svg" class="avatar avatar-ex-sm" alt="">
+                                                    </div>
+                                                    <div class="content">
+                                                        <h4 class="title mb-0">Mã nguồn mạnh mẽ</h4>
+                                                        <p class="text-muted mb-0">Xây dựng trên mã nguồn Wordpress update thường xuyên</p>    
+                                                    </div>
+                                                </div>
+                                            </a>
+                                        </div><!--end col-->
+                                        
+                                        <div class="col-lg-6 col-md-6 col-12 mt-4 pt-2">
+                                            <a class="text-dark" id="button-price-ads-remove">
+                                                <div class="key-feature d-flex p-3 bg-white rounded shadow">
+                                                    <div class="icons text-center rounded-pill mr-3 mt-2">
+                                                        <img src="images/remove.svg" class="avatar avatar-ex-sm" alt="">
+                                                    </div>
+                                                    <div class="content">
+                                                        <h4 class="title mb-0">Chỉnh sửa dễ dàng</h4>
+                                                        <p class="text-muted mb-0">Admin theme giúp bạn dễ dàng chỉnh sửa mọi phần trên wesite</p>    
+                                                    </div>
+                                                </div>
+                                            </a>
+                                        </div><!--end col-->
+                                        
+                                        <div class="col-lg-6 col-md-6 col-12 mt-4 pt-2">
+                                            <a class="text-dark" id="button-price-automation">
+                                                <div class="key-feature d-flex p-3 bg-white rounded shadow">
+                                                    <div class="icons text-center rounded-pill mr-3 mt-2">
+                                                        <img src="images/automation.svg" class="avatar avatar-ex-sm" alt="">
+                                                    </div>
+                                                    <div class="content">
+                                                        <h4 class="title mb-0">Đồng bộ CRMLAND</h4>
+                                                        <p class="text-muted mb-0">Giúp bạn lưu trữ thông tin và khai thác khách hàng hiệu quả</p>    
+                                                    </div>
+                                                </div>
+                                            </a>
+                                        </div><!--end col-->
+                                        
+                                        <div class="col-lg-6 col-md-6 col-12 mt-4 pt-2">
+                                            <a class="text-dark" id="button-price-brandname-create">
+                                                <div class="key-feature d-flex p-3 bg-white rounded shadow">
+                                                    <div class="icons text-center rounded-pill mr-3 mt-2">
+                                                        <img src="images/brand.svg" class="avatar avatar-ex-sm" alt="">
+                                                    </div>
+                                                    <div class="content">
+                                                        <h4 class="title mb-0">Khởi tạo nhanh chóng</h4>
+                                                        <p class="text-muted mb-0">Chỉ với vài click chuột là bạn đã có 1 website giống demo</p>    
+                                                    </div>
+                                                </div>
+                                            </a>
+                                        </div><!--end col-->
+                                        
+                                        <div class="col-lg-6 col-md-6 col-12 mt-4 pt-2">
+                                            <a class="text-dark" id="button-price-advance-services">
+                                                <div class="key-feature d-flex p-3 bg-white rounded shadow">
+                                                    <div class="icons text-center rounded-pill mr-3 mt-2">
+                                                        <img src="images/customer.svg" class="avatar avatar-ex-sm" alt="">
+                                                    </div>
+                                                    <div class="content">
+                                                        <h4 class="title mb-0">Miễn phí và miễn phí</h4>
+                                                        <p class="text-muted mb-0">Và trên hết, mẫu giao diện này là miễn phí 100% với bạn</p>    
+                                                    </div>
+                                                </div>
+                                            </a>
+                                        </div><!--end col-->
+                                    </div>
+                                    <h3 class="text-danger mt-5">Bạn không nhầm đâu - 100/100 điểm Google Insight</h3>
+                                    <p>Mẫu giao diện này được đội ngũ kỹ thuật viên của chúng tôi tối ưu từng thành phần, giúp bạn đạt kết quả tối ưu với Google. Điều này rất tốt cho việc SEO website lên google hay bạn chạy Adwords</p>
+                                    <img src="images/google-100.png" class="img-fluid mb-3">
+                                    <p>Check online: https://google.com</p>
+                                    <h3 class="text-danger mt-5">Dễ dàng tùy chỉnh mọi thành phần trên website</h3>
+                                    <p>Đúng vậy ! Bạn có thể cá nhân hóa mẫu website của bạn trong theme option. Chúng tôi tối ưu hóa mọi thứ nhằm trải nghiệm của bạn dễ dàng nhất. Bạn có thể tìm thấy mục này trong trang Admin / WEBLAND </p>
+                                    <h3 class="text-danger mt-5">Tạo Website tự động, có ngay web sau 2 phút</h3>
+                                    <p>Đúng vậy, công cụ tạo website tự động sẽ giúp bạn có ngay website giống 100% demo sau 2 phút. Không cần chờ đợi, không cần nhờ kỹ thuật hỗ trợ. Bạn có thể làm tất cả việc này vì nó quá đơn giản</p>
+                                    <h3 class="text-danger mt-5">Đồng bộ tất cả với CRMLAND</h3>
+                                    <p>Trong trang quản trị, bạn có thể dễ dạng tạo các popup, form lấy thông tin khách hàng trên website của bạn. Dữ liệu khách hàng sẽ tự động đồng bộ mà không cần bạn phải thao tác bất cứ điều gì</p>
+                                    <h3 class="text-danger mt-5">Và trên hết, mọi thứ đều miễn phí</h3>
+                                    <p> Bạn sẽ được tận dụng mọi thứ miễn phí: Tạo website, thay đổi giao diện, đổi tên miền ...</p>
+                                    <p> Tuy Nhiên, các website miễn phí bạn sẽ có 1 khung nhỏ chứa quảng cáo của chúng tôi. Nếu bạn muốn loại bỏ quảng cáo này, bạn sẽ mất một chút chi phí</p>
+                               </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- BLog End -->
+
+                    
+                </div><!--end row-->
+            </div><!--end container-->
+        </section><!--end section-->
+        <!-- Blog End -->
+<?php include 'footer.php';?>
